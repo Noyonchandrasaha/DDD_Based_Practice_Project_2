465e4972b845242eaa0a56e1714753e685f08079
Noyonchandrasaha <noyonsaha001@gmail.com>
2025-11-08 14:04:40 +0600

    chore: write UserEmail.vo.ts code

--- src/modules/user/domain/value-objects/UserEmail.vo.ts
+++ src/modules/user/domain/value-objects/UserEmail.vo.ts
@@ -2,3 +2,39 @@
 // FILE: src/modules/user/domain/value-objects/Email.vo.ts
 // ============================================================
 
+import { ValueObject } from '@core/domain/value-objects/ValueObject';
+import { REGEX_PATTERNS } from '@common/constants/regex.constants';
+
+export interface UserEmailProps {
+    email: string;
+}
+
+export class UserEmailVO extends ValueObject<UserEmailProps> {
+    private constructor(props: UserEmailProps) {
+        super(props)
+    }
+
+    get email(): string{
+        return this.props.email
+    }
+
+    private static validate(props: UserEmailProps): void {
+        if (!props.email?.trim()) {
+        throw new Error('Email cannot be empty.');
+        }
+
+        const emailRegex = REGEX_PATTERNS.EMAIL;
+        if (!emailRegex.test(props.email)) {
+        throw new Error(`Invalid email format: ${props.email}`);
+        }
+    }
+
+    public static create(email: string): UserEmailVO {
+        const normalizedEmail = email.trim().toLowerCase();
+
+        const props: UserEmailProps = { email: normalizedEmail };
+        UserEmailVO.validate(props);
+
+        return new UserEmailVO(props);
+    }
+}
