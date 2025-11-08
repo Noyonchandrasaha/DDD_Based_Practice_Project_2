f367b88efc33b34f906cdcb512bdde388ca1a94e
Noyonchandrasaha <noyonsaha001@gmail.com>
2025-11-08 16:55:23 +0600

    chore: write the userCreate.event.ts file

--- src/modules/user/domain/events/UserCreated.event.ts
+++ src/modules/user/domain/events/UserCreated.event.ts
@@ -2,3 +2,70 @@
 // FILE: src/modules/user/domain/events/UserCreated.event.ts
 // ============================================================
 
+import { BaseDomainEvent } from '@core/domain/events/BaseDomainEvent';
+
+export interface UserCreatedPayload {
+    userId: string;
+    fullname: string;
+    email: string;
+    phoneNumber: string;
+    street: string;
+    city: string;
+    state?: string;
+    postalCode?: string;
+    country: string;
+    isActive: boolean;
+    isDeleted: boolean;
+}
+
+export class UserCreatedEvent extends BaseDomainEvent<UserCreatedPayload> {
+    public static readonly EVENT_NAME = 'UserCreated';
+
+    constructor(payload: UserCreatedPayload, metadata?: {correlationId?: string}) {
+        super(UserCreatedEvent.EVENT_NAME, payload, metadata)
+    }
+
+    get userId(): string {
+        return this.payload.userId;
+    }
+
+    get fullName(): string {
+        return this.payload.fullname;
+    }
+
+    get email(): string {
+        return this.payload.email;
+    }
+
+    get phoneNumber(): string {
+        return this.payload.phoneNumber;
+    }
+
+    get street(): string {
+        return this.payload.street;
+    }
+
+    get city(): string {
+        return this.payload.city;
+    }
+    
+    get state(): string | undefined {
+        return this.payload.state;
+    }
+
+    get postalCode(): string | undefined {
+        return this.payload.postalCode;
+    }
+
+    get conuntry(): string {
+        return this.payload.country;
+    }
+
+    get isActive(): boolean {
+        return this.payload.isActive;
+    }
+
+    get isDeleted(): boolean {
+        return this.payload.isDeleted;
+    }
+}
