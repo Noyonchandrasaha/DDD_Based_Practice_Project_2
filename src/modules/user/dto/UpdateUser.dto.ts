58d77b5858f662fa6e1ea857e2ae5c22e666b9a3
Noyonchandrasaha <noyonsaha001@gmail.com>
2025-11-10 11:21:57 +0600

    chore: write the Updated user dto code

--- src/modules/user/dto/UpdateUser.dto.ts
+++ src/modules/user/dto/UpdateUser.dto.ts
@@ -2,3 +2,36 @@
 // FILE: src/modules/user/dto/UpdateUser.dto.ts
 // ============================================================
 
+import { UpdateUserInput } from '../validator/UpdateUser.validator';
+
+export class UpdateUserDTO{
+    private constructor(
+        public readonly firstName?: string | null,
+        public readonly middleName?: string | null,
+        public readonly lastName?: string | null,
+        public readonly email?: string | null,
+        public readonly phoneNumber ?: string | null,
+        public readonly street ?: string | null,
+        public readonly city ?: string | null,
+        public readonly state ?: string | null,
+        public readonly postalCode ?: string | null,
+        public readonly country ?: string | null,
+        public readonly isActive ?: boolean
+    ) {}
+
+    public static fromValidatedDate (data: UpdateUserInput): UpdateUserDTO{
+        return new UpdateUserDTO(
+            data.firstName,
+            data.middleName,
+            data.lastName,
+            data.email,
+            data.phoneNumber,
+            data.street,
+            data.city,
+            data.state,
+            data.postalCode,
+            data.country,
+            data.isActive
+        )
+    }
+}
