1ae1bdd0aa1d09da936959ea7ee09cc1d8ee55ce
Noyonchandrasaha <noyonsaha001@gmail.com>
2025-11-10 15:52:34 +0600

    chore: write the Get User usecase code

--- src/modules/user/application/use-cases/GetUser.usecase.ts
+++ src/modules/user/application/use-cases/GetUser.usecase.ts
@@ -2,3 +2,28 @@
 // FILE: src/modules/user/application/use-cases/GetUser.usecase.ts
 // ============================================================
 
+import { User } from '../../domain/User.entity';
+import { UserRepository } from '../../repository/User.repository';
+
+type GetUserOptions = {
+    includeDeleted?: boolean;
+}
+
+export class GetUserUseCase {
+    constructor (private readonly userRepo: UserRepository){}
+
+    async execute(id: string, options: GetUserOptions = {}): Promise<User> {
+        const {includeDeleted = false} = options;
+
+        const user = await this.userRepo.findById(id);
+        if(!user){
+            throw new Error('User not found');
+        }
+
+        const deletedAt = (user as any).deletedAt ?? null;
+        if(!includeDeleted && deletedAt) {
+            throw new Error('User not found.')
+        }
+        return user;
+    }
+}
