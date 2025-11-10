a1e7375e1e45bd46d463eae591f27b41b0503d42
Noyonchandrasaha <noyonsaha001@gmail.com>
2025-11-10 11:15:49 +0600

    chore: write the crateUser dto code

--- src/modules/user/dto/CreateUser.dto.ts
+++ src/modules/user/dto/CreateUser.dto.ts
@@ -1,3 +1,32 @@
 // ============================================================
 // FILE: src/modules/user/dto/CreateUser.dto.ts
 // ============================================================
+import { CreateUserInput } from '../validator/CreateUser.validator'
+
+export class CreateuserDTO {
+    private constructor (
+        public readonly firstName: string | undefined,
+        public readonly middleName: string | undefined,
+        public readonly lastName: string,
+        public readonly email: string,
+        public readonly phoneNumber: string,
+        public readonly street: string,
+        public readonly city: string,
+        public readonly state: string | undefined,
+        public readonly country: string
+    ) {}
+
+    public static fromValidatedData(data: CreateUserInput): CreateuserDTO {
+        return new CreateuserDTO(
+            data.firstName,
+            data.middleName,
+            data.lastName,
+            data.email,
+            data.phoneNumber,
+            data.street,
+            data.city,
+            data.state,
+            data.country
+        )
+    }
+}
