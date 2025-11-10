1e4be6c8aa8ad1c6bd215699b413a30fa18a338f
Noyonchandrasaha <noyonsaha001@gmail.com>
2025-11-10 14:36:03 +0600

    chore: write the create User handler

--- src/modules/user/application/commands/CreateUser.handler.ts
+++ src/modules/user/application/commands/CreateUser.handler.ts
@@ -1,3 +1,28 @@
 // ============================================================
 // FILE: src/modules/user/application/handlers/CreateUser.handler.ts
 // ============================================================
+
+import { User } from '../../domain/User.entity';
+import { CreateUserCommand } from './CreateUser.command';
+import { UserMapper } from '../../mappers/User.mapper';
+import { CreateuserDTO } from '../../dto/CreateUser.dto';
+
+export class CreateUserHandler {
+    async execute(command: CreateUserCommand): Promise<User> {
+        const dto = CreateuserDTO.fromValidatedData({
+            firstName: command.firstName ?? undefined,
+            middleName: command.middleName ?? undefined,
+            lastName: command.lastName,
+            email: command.email,
+            phoneNumber: command.phoneNumber,
+            street: command.street,
+            city: command.city,
+            state: command.state ?? undefined,
+            postalCode : command.postalCode ?? undefined,
+            country: command.country
+        });
+
+        const props = UserMapper.toDomainprops(dto);
+        return User.create(props)
+    }
+}
