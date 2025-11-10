f3566d0548e0999694ce9396af7454f078c78316
Noyonchandrasaha <noyonsaha001@gmail.com>
2025-11-10 11:44:08 +0600

    chore: write the paginated user response code

--- src/modules/user/dto/PaginatedUserResponse.dto.ts
+++ src/modules/user/dto/PaginatedUserResponse.dto.ts
@@ -2,3 +2,34 @@
 // FILE: src/modules/user/dto/PaginatedUserResponse.dto.ts
 // ============================================================
 
+import { UserResponseDTO } from './UserResponse.dto';
+
+export class PaginatedUserResponseDTO {
+    private constructor(
+        public readonly data: UserResponseDTO[],
+        public readonly meta: {
+            totalItems: number,
+            totalPages: number,
+            currentPage: number,
+            pageSize: number
+        }
+    ) {}
+
+    public static create(
+        data: UserResponseDTO[],
+        totalItems: number,
+        totalPages: number,
+        currentPage: number,
+        pageSize: number
+    ): PaginatedUserResponseDTO {
+        return new PaginatedUserResponseDTO(
+            data,
+            {
+                totalItems,
+                totalPages,
+                currentPage,
+                pageSize
+            }
+        )
+    }
+}
